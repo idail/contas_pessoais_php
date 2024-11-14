e3d26536cc4dced6d3226adc583250a49fab32df
idail <neto_br_8@hotmail.com>
2024-11-13 23:11:10 -0400

    Implementações
    Finalizada a implementação do cadastramento de usuario na api

--- API/Usuario.php
+++ API/Usuario.php
@@ -4,16 +4,32 @@ header('Access-Control-Allow-Origin: *');
 //Especifica um ou mais métodos permitidos ao acessar um recurso em resposta a uma solicitação de comprovação
 header("Access-Control-Allow-Methods: POST , GET , DELETE");
 header("Content-Type: application/json; charset=UTF-8");
-
+require("Conexao.php");
 if($_SERVER["REQUEST_METHOD"] === "POST")
 {
     $valores = json_decode(file_get_contents("php://input"), true);
 
     $nomeUsuario = $valores["nome_usuario"];
+    $loginUsuario = $valores["login_usuario"];
+    $emailUsuario = $valores["email_usuario"];
+    $senhaUsuario = $valores["senha_usuario"];
     $nomeImagemUsuario = $valores["nome_imagem"];
 
-    $dados = $nomeUsuario."-".$nomeImagemUsuario;
+    $senhaCriptografada = md5($senhaUsuario);
+    
+    //$dados = $nomeUsuario."\n".$loginUsuario."\n".$emailUsuario."\n".$senhaUsuario."\n".$nomeImagemUsuario;
 
-    echo json_encode($dados);
+    $sqlCadastrarUsuario = "insert into usuario(nome_usuario,login_usuario,email_usuario,senha_usuario,imagem_usuario)values
+    (:recebe_nome_usuario,:recebe_login_usuario,:recebe_email_usuario,:recebe_senha_usuario,:recebe_imagem_usuario)";
+    $comandoCadastrarUsuaruio = Conexao::Obtem()->prepare($sqlCadastrarUsuario);
+    $comandoCadastrarUsuaruio->bindValue(":recebe_nome_usuario",$nomeUsuario);
+    $comandoCadastrarUsuaruio->bindValue(":recebe_login_usuario",$loginUsuario);
+    $comandoCadastrarUsuaruio->bindValue(":recebe_email_usuario",$emailUsuario);
+    $comandoCadastrarUsuaruio->bindValue(":recebe_senha_usuario",$senhaCriptografada);
+    $comandoCadastrarUsuaruio->bindValue(":recebe_imagem_usuario",$nomeImagemUsuario);
+    $comandoCadastrarUsuaruio->execute();
+    $registroUltimoCodigoCadastroUsuario = Conexao::Obtem()->lastInsertId();
+
+    echo json_encode($registroUltimoCodigoCadastroUsuario);
 }
 ?>
