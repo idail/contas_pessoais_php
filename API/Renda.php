42366e6c5e3369e22e0d4115552df80ea4c54266
idail <neto_br_8@hotmail.com>
2024-12-29 17:46:06 -0400

    Ajustado a renda para exibir apenas a renda de cada usuario

--- API/Renda.php
+++ API/Renda.php
@@ -20,13 +20,15 @@ if($_SERVER["REQUEST_METHOD"] === "POST")
         $categoriaRenda = $valores["categoria_renda"];
         $valorRenda = $valores["valor_renda"];
         $pagoRenda = $valores["pago_renda"];
+        $usuarioCodigoRenda = $valores["codigo_renda"];
 
-        $sqlCadastrarRenda = "insert into renda(nome_renda,categoria_renda,valor_renda,pago_renda)values(:recebe_nome_renda,:categoria_renda,:valor_renda,:pago_renda)";
+        $sqlCadastrarRenda = "insert into renda(nome_renda,categoria_renda,valor_renda,pago_renda,codigo_usuario)values(:recebe_nome_renda,:recebe_categoria_renda,:recebe_valor_renda,:recebe_pago_renda,:recebe_codigo_usuario)";
         $comandoCadastrarRenda = Conexao::Obtem()->prepare($sqlCadastrarRenda);
         $comandoCadastrarRenda->bindValue(":recebe_nome_renda",$nomeRenda);
-        $comandoCadastrarRenda->bindValue(":categoria_renda",$categoriaRenda);
-        $comandoCadastrarRenda->bindValue(":valor_renda",$valorRenda);
-        $comandoCadastrarRenda->bindValue(":pago_renda",$pagoRenda);
+        $comandoCadastrarRenda->bindValue(":recebe_categoria_renda",$categoriaRenda);
+        $comandoCadastrarRenda->bindValue(":recebe_valor_renda",$valorRenda);
+        $comandoCadastrarRenda->bindValue(":recebe_pago_renda",$pagoRenda);
+        $comandoCadastrarRenda->bindValue(":recebe_codigo_usuario",$usuarioCodigoRenda);
         $comandoCadastrarRenda->execute();
         $registroUltimoCodigoCadastroRenda = Conexao::Obtem()->lastInsertId();
 
@@ -38,15 +40,17 @@ if($_SERVER["REQUEST_METHOD"] === "POST")
         $valorRenda = $valores["valor_renda"];
         $pagoRenda = $valores["pago_renda"];
         $codigoRenda = $valores["codigo_renda"];
+        $codigoUsuarioRenda = $valores["codigo_usuario_renda"];
 
-        $sqlCadastrarRenda = "update renda set nome_renda = :recebe_nome_renda, categoria_renda = :recebe_categoria_renda, valor_renda = :recebe_valor_renda, pago_renda = :recebe_pago_renda where codigo_renda = :recebe_codigo_renda";
-        $comandoCadastrarRenda = Conexao::Obtem()->prepare($sqlCadastrarRenda);
-        $comandoCadastrarRenda->bindValue(":recebe_nome_renda",$nomeRenda);
-        $comandoCadastrarRenda->bindValue(":recebe_categoria_renda",$categoriaRenda);
-        $comandoCadastrarRenda->bindValue(":recebe_valor_renda",$valorRenda);
-        $comandoCadastrarRenda->bindValue(":recebe_pago_renda",$pagoRenda);
-        $comandoCadastrarRenda->bindValue(":recebe_codigo_renda",$codigoRenda);
-        $resultadoAlterarRenda = $comandoCadastrarRenda->execute();
+        $sqlCadastrarRenda = "update renda set nome_renda = :recebe_nome_renda, categoria_renda = :recebe_categoria_renda, valor_renda = :recebe_valor_renda, pago_renda = :recebe_pago_renda where codigo_renda = :recebe_codigo_renda and codigo_usuario = :recebe_codigo_usuario";
+        $comandoAlterarRenda = Conexao::Obtem()->prepare($sqlCadastrarRenda);
+        $comandoAlterarRenda->bindValue(":recebe_nome_renda",$nomeRenda);
+        $comandoAlterarRenda->bindValue(":recebe_categoria_renda",$categoriaRenda);
+        $comandoAlterarRenda->bindValue(":recebe_valor_renda",$valorRenda);
+        $comandoAlterarRenda->bindValue(":recebe_pago_renda",$pagoRenda);
+        $comandoAlterarRenda->bindValue(":recebe_codigo_renda",$codigoRenda);
+        $comandoAlterarRenda->bindValue(":recebe_codigo_usuario",$codigoUsuarioRenda);
+        $resultadoAlterarRenda = $comandoAlterarRenda->execute();
         
         if($resultadoAlterarRenda)
             echo json_encode("renda alterada");
@@ -58,18 +62,21 @@ if($_SERVER["REQUEST_METHOD"] === "POST")
     $processo_renda = $_GET["execucao"];
     $opcao = $_GET["opcao"];
     $filtroInformado = $_GET["filtro"];
+    $codigoUsuarioRenda = $_GET["codigo_usuario_renda"];
     if($processo_renda === "busca_rendas")
     {
         if($opcao === "todos")
         {
-            $sqlBuscaRendas = "select * from renda";
+            $sqlBuscaRendas = "select * from renda where codigo_usuario = :recebe_codigo_usuario";
             $comandoBuscaRendas = Conexao::Obtem()->prepare($sqlBuscaRendas);
+            $comandoBuscaRendas->bindValue(":recebe_codigo_usuario",$codigoUsuarioRenda);
             $comandoBuscaRendas->execute();
             $resultadoBuscaRendas = $comandoBuscaRendas->fetchAll(PDO::FETCH_ASSOC);
         }else{
-            $sqlBuscaRendas = "select * from renda where nome_renda like :recebe_nome_renda";
+            $sqlBuscaRendas = "select * from renda where nome_renda like :recebe_nome_renda and codigo_usuario = :recebe_codigo_usuario";
             $comandoBuscaRendas = Conexao::Obtem()->prepare($sqlBuscaRendas);
             $comandoBuscaRendas->bindValue(":recebe_nome_renda","%$filtroInformado%");
+            $comandoBuscaRendas->bindValue(":recebe_codigo_usuario",$codigoUsuarioRenda);
             $comandoBuscaRendas->execute();
             $resultadoBuscaRendas = $comandoBuscaRendas->fetchAll(PDO::FETCH_ASSOC);
         }
@@ -98,3 +105,4 @@ if($_SERVER["REQUEST_METHOD"] === "POST")
             echo json_encode("renda nao excluida");
     }
 }
+?>
